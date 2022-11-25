ebdf5d5cec7b24d06c54191c4f51bfd639e0fec2
miyazaDaniels <55.miyazawa@gmail.com>
2022-11-25 12:51:56 +0900

    feat: data loading

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,6 +6,9 @@
           <WorkItem :work="work"></WorkItem>
         </v-col>
       </v-row>
+      <div v-if="hasMoreData" v-intersect="onIntersect" class="loading">
+        <v-progress-circular v-if="isLoading" indeterminate />
+      </div>
     </v-container>
   </div>
 </template>
@@ -13,6 +16,7 @@
 <script>
 import { createClient } from "microcms-js-sdk";
 import WorkItem from "../components/WorkItem.vue";
+const FETCH_LIMIT = 10;
 
 export default {
   name: "HomePage",
@@ -22,6 +26,9 @@ export default {
   data() {
     return {
       works: [],
+      isLoading: false,
+      hasMoreData: true,
+      offset: 0,
     };
   },
   created() {
@@ -30,20 +37,45 @@ export default {
       apiKey: "fb80446547044911b4e4273a2034314f0820",
     });
   },
-  mounted() {
-    this.updateWorks();
-  },
   methods: {
     updateWorks() {
       this.client
         .get({
           endpoint: "works",
+          queries: { offset: this.offset }
         })
         .then((res) => {
-          console.log(res);
-          this.works = res.contents;
+          this.works = this.works.concat(res.contents);
+          this.isLoading = false;
+          this.updateOffset(res.totalCount);
+        })
+        .catch((err) => {
+          console.log(err);
+          this.isLoading = false;
+          this.offset = 0;
+          this.hasMoreData = false;
         });
+    },
+    updateOffset(totalCount) {
+      if (this.offset > totalCount) {
+        this.hasMoreData = false;
+      } else {
+        this.offset += FETCH_LIMIT;
+      }
+    },
+    onIntersect(entries, observer, isIntersecting) {
+      if (!isIntersecting) {
+        return;
+      }
+      this.isLoading = true;
+      this.updateWorks();
     },
   },
 };
 </script>
+<style scoped>
+.loading {
+  margin: 10px;
+  text-align: center;
+}
+</style>
